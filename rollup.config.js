5e49febd2988b7d9105730093fdf37c0677ea8c0
Moritz Willy Becher <git@wc.becher.io>
2021-03-01 21:45:02 +0100

    add quicktype and pass parameters

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import pkg from './package.json';
 export default {
   input: 'src/index.ts',
   plugins: [typescript({ sourceMap: false })],
-  external: ['path', 'fs', 'typescript'],
+  external: ['quicktype'],
   output: [
     { format: 'cjs', file: pkg.main, exports: 'auto' },
     { format: 'esm', file: pkg.module }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,45 @@
-import * as path from 'path';
 
-import { Plugin, RollupOptions, SourceDescription } from 'rollup';
+import { Plugin } from 'rollup';
 
-class RollupQuicktypeOptions {
+import { RendererOptions } from "quicktype/dist/quicktype-core";
+import { CLIOptions, main } from 'quicktype';
 
+class RollupQuicktypeOptions implements CLIOptions
+{
+    lang: string;
+    topLevel: string;
+    src: string[];
+    srcUrls?: string;
+    srcLang: string;
+    additionalSchema: string[];
+    graphqlSchema?: string;
+    graphqlIntrospect?: string;
+    httpHeader?: string[];
+    httpMethod?: string;
+    out?: string;
+    buildMarkovChain?: string;
+
+    alphabetizeProperties: boolean;
+    allPropertiesOptional: boolean;
+    noRender: boolean;
+
+    rendererOptions: RendererOptions;
+
+    help: boolean;
+    quiet: boolean;
+    version: boolean;
+    debug?: string;
+    telemetry?: string;
+
+    // We use this to access the inference flags
+    [option: string]: any;
 }
 
-export default function quicktype(options: RollupQuicktypeOptions = {}): Plugin {
+export default function quickTypeRollup(options: Partial<RollupQuicktypeOptions>): Plugin {
     return {
-        name: 'quicktypes'
+        name: 'quicktypes',
+        generateBundle() {
+            main(options);
+        }
     }
 }
