182fdc1ec64fc849145414f90e2e1d94fc4c1fbe
Moritz Willy Becher <git@wc.becher.io>
2021-03-06 12:58:00 +0100

    use rollup transform function for quicktype

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import pkg from './package.json';
 export default {
   input: 'src/index.ts',
   plugins: [typescript({ sourceMap: false })],
-  external: ['quicktype'],
+  external: ['quicktype', '@rollup/pluginutils', 'quicktype/dist/quicktype-core'],
   output: [
     { format: 'cjs', file: pkg.main, exports: 'auto' },
     { format: 'esm', file: pkg.module }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 
 import { Plugin } from 'rollup';
 
-import { CLIOptions, main } from 'quicktype';
+import { CLIOptions, main, makeQuicktypeOptions } from 'quicktype';
+import { quicktypeMultiFile } from 'quicktype/dist/quicktype-core'
+
+import { createFilter, FilterPattern } from '@rollup/pluginutils';
+
 
 class RollupQuicktypeOptions implements Partial<CLIOptions>
 {
-    lang: string;
+    include: FilterPattern;
+    exclude: FilterPattern;
+
+    lang: string = "javascript";
     topLevel: string;
     src: string[];
     srcUrls?: string;
-    srcLang: string;
+    srcLang: string = "schema";
     additionalSchema: string[];
     graphqlSchema?: string;
     graphqlIntrospect?: string;
@@ -33,8 +40,57 @@ class RollupQuicktypeOptions implements Partial<CLIOptions>
 }
 
 export default function quicktype(options: RollupQuicktypeOptions): Plugin {
+    const filter = createFilter(options.include, options.exclude);
+
     return {
         name: 'quicktypes',
+        async transform(json, id) {
+            if (id.slice(-12) !== '.schema.json' || !filter(id)) return null;
+
+            const cliOptions: CLIOptions = {
+                src: options.src || [],
+                srcUrls: options.srcUrls,
+                srcLang: options.srcLang,
+                lang: options.lang,
+                topLevel: id,
+                noRender: !!options.noRender,
+                alphabetizeProperties: !!options.alphabetizeProperties,
+                allPropertiesOptional: !!options.allPropertiesOptional,
+                rendererOptions: options.rendererOptions || {},
+                help: options.help || false,
+                quiet: options.quiet || false,
+                version: options.version || false,
+                out: options.out,
+                buildMarkovChain: options.buildMarkovChain,
+                additionalSchema: options.additionalSchema || [],
+                graphqlSchema: options.graphqlSchema,
+                graphqlIntrospect: options.graphqlIntrospect,
+                httpMethod: options.httpMethod,
+                httpHeader: options.httpHeader,
+                debug: options.debug,
+                telemetry: options.telemetry
+            };
+      
+            const quicktypeOptions = await makeQuicktypeOptions(cliOptions);
+            if (quicktypeOptions === undefined) {
+                return null;
+            }
+        
+            const resultsByFilename = await quicktypeMultiFile(quicktypeOptions);
+
+            let code = "";
+            for(const [filename, { lines, annotations }] of resultsByFilename)
+            {
+                code += lines.join("\n");
+                code += "\n";
+            }
+
+            return {
+                code: code,
+                map: { mappings: '' }
+            };
+
+          },
         generateBundle() {
             main(options);
         }
